8cb1bea1e59babe38fe863442f7d967699b530ef
Tore Fauchald <tore.fauchald@gmail.com>
2024-11-25 10:22:38 +0100

    Update GUI and Config

--- gui.py
+++ gui.py
@@ -12,14 +12,14 @@ def generate_file():
         return
     
     with open("config.txt", "w") as file:
-        file.write("### Welcome to the Planetary Positioning Project! ###\n")
-        file.write("# Modify the parameters below as needed. Ensure:\n")
-        file.write("# - The date follows the dd.mm.yyyy format.\n")
-        file.write("# - All parameters are enclosed in single quotes.\n")
+        #file.write("### Welcome to the Planetary Positioning Project! ###\n")
+        #file.write("# Modify the parameters below as needed. Ensure:\n")
+        #file.write("# - The date follows the dd.mm.yyyy format.\n")
+        #file.write("# - All parameters are enclosed in single quotes.\n")
         file.write(f"Date: '{date}'\n")
         file.write(f"Text: '{text}'\n")
     
-    messagebox.showinfo("Success", "The config.txt file has been generated.\n\nPlease click on the [Make 3D-print file] button after closing this window")
+    messagebox.showinfo("Success", "The config.txt file has been generated.\n\nPlease click on the [Create STL & plot-files] button after closing this window")
     #root.quit()  # Close the main window after execution
 
 def run_main(): 
@@ -27,7 +27,7 @@ def run_main():
         # Activate the virtual environment and run main.py 
         venv_path = os.path.join(os.getcwd(), '.venv', 'Scripts', 'python') 
         subprocess.run([venv_path, "main.py"], check=True) 
-        messagebox.showinfo("Success", "You have now a file named: plot_3D.eps on your computer.") 
+        messagebox.showinfo("Success", "You have now these files on your computer: \n plot_3D.png, plot_2D.png & solar_system.stl") 
         root.quit()  # Close the main window after execution
     except subprocess.CalledProcessError as e: 
         messagebox.showerror("Error", f"An error occurred: {e}")
@@ -37,11 +37,11 @@ root = tk.Tk()
 root.title("Input info")
 
 # Add instruction text
-instruction_label = tk.Label(root, text="Please add your desired date and name in the boxes below.")
+instruction_label = tk.Label(root, text="Please add your desired date and text in the boxes below.\n Please use date format as (dd.mm.yyyy)")
 instruction_label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
 # Create and place the date label and entry
-tk.Label(root, text="Date (dd.mm.yyyy):").grid(row=1, column=0, padx=10, pady=10)
+tk.Label(root, text="Date:").grid(row=1, column=0, padx=10, pady=10)
 date_entry = tk.Entry(root)
 date_entry.grid(row=1, column=1, padx=10, pady=10)
 
@@ -55,7 +55,7 @@ generate_button = tk.Button(root, text="Save Information", command=generate_file
 generate_button.grid(row=3, column=0, columnspan=2, pady=10)
 
 # Create and place the run button
-run_button = tk.Button(root, text="Make 3D-print file", command=run_main)
+run_button = tk.Button(root, text="Create STL & plot-files", command=run_main)
 run_button.grid(row=4, column=0, columnspan=2, pady=10)
 
 # Run the application
